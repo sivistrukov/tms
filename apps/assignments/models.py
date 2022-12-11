0d7d5d5124f1a56f4dae277dce2bb714532d2a1f
Vladislav <80812956+sivistrukov@users.noreply.github.com>
2022-12-11 21:07:47 +0900

    feat(assignments): start app
    add models and base templates

---
+++ apps/assignments/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from apps.counterparties.models import Counterparty
+
+
+class Assignment(models.Model):
+    class Status(models.TextChoices):
+        AWAITING_PAYMENT = 'Ожидает оплату', 'Ожидает оплату'
+        PAID = 'Оплачен', 'Оплачен'
+        CANCELED = 'Отменен', 'Отменен'
+
+    loading_address = models.CharField(max_length=150)
+    unloading_address = models.CharField(max_length=150)
+    status = models.CharField(max_length=50, choices=Status.choices)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    customers = models.ForeignKey(Counterparty, related_name='assignments',
+                                  on_delete=models.CASCADE)
+    representative = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=12)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Payload(models.Model):
+    name = models.CharField(max_length=150)
+    count = models.IntegerField()
+    assignment = models.ForeignKey(Assignment, related_name='payloads',
+                                   on_delete=models.CASCADE)
--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
     'apps.accounts.apps.AccountsConfig',
     'apps.counterparties.apps.CounterpartiesConfig',
+    'apps.assignments.apps.AssignmentsConfig',
 ]
 
 MIDDLEWARE = [
